fd0586554ab6d4d83b47a38ce1d649076553bbec
OlguinJv <olguin.j.vito@gmail.com>
2019-01-26 03:24:55 -0300

    fast commit

--- js/scripts.js
+++ js/scripts.js
@@ -21,15 +21,25 @@ function hideOnResize() {
 
 }
 
+var menuLinks = document.getElementsByClassName('burger-link')
+function activateMenuButtons(){
+
+    for (var i = 0; i < menuLinks.length; i++){
+        menuLinks[i].addEventListener('click', function(e){
+            toggleMenu();
+        })
+    }
+}
+
 var scrollIdx = 0;
 function isScrolledIntoView(el) {
 
     if (scrollIdx == el.length) return;
 
-    var actualElement =  document.getElementById(el[scrollIdx]),
-    rect = actualElement.getBoundingClientRect(),
-    elemTop = rect.top,
-    elemBottom = rect.bottom;
+    var actualElement = document.getElementById(el[scrollIdx]),
+        rect = actualElement.getBoundingClientRect(),
+        elemTop = rect.top,
+        elemBottom = rect.bottom;
 
     // Only completely visible elements return true:
     // var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
@@ -42,10 +52,49 @@ function isScrolledIntoView(el) {
     }
 }
 
+var menuButtons = ['btn-estudio', 'btn-servicios', 'btn-proyectos', 'btn-contacto'],
+    btnSelected = '#e7205b',
+    btnUnselected = '#4a4a4a';
+
+function checkVisibleSection(elem) {
+
+    var actualElement = document.getElementById(`mark-${elem}`),
+        rect = actualElement.getBoundingClientRect(),
+        elemTop = rect.top,
+        elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    // var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    var isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    if (isVisible) {
+        paintButton(`btn-${elem}`)
+    }
+
+}
+
+function paintButton(elem) {
+    for (var i = 0; i < menuButtons.length; i++) {
+        if (menuButtons[i] == elem) {
+            document.getElementById(menuButtons[i]).style.color = btnSelected;///TO-DO
+        } else {
+            document.getElementById(menuButtons[i]).style.color = btnUnselected;///TO-DO
+        }
+    }
+}
+
 function onResizeAction() {
     hideOnResize();
 }
 
 function onScrollAction() {
-    isScrolledIntoView(['first-line', 'second-line', 'third-line'])
+    isScrolledIntoView(['first-line', 'second-line', 'third-line']);
+    checkVisibleSection('estudio');
+    checkVisibleSection('servicios');
+    checkVisibleSection('proyectos');
+    checkVisibleSection('contacto');
+}
+
+window.onload = function(){
+    activateMenuButtons();
 }
