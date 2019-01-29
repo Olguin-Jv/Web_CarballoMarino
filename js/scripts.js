58497ab4622c68e900d2bd5fd08c6111bfe46143
OlguinJv <olguin.j.vito@gmail.com>
2019-01-29 04:35:42 -0300

    fast commit

--- js/scripts.js
+++ js/scripts.js
@@ -31,72 +31,8 @@ function activateMenuButtons(){
     }
 }
 
-var scrollIdx = 0;
-function isScrolledIntoView(el) {
-
-    if (scrollIdx == el.length) return;
-
-    var actualElement = document.getElementById(el[scrollIdx]),
-        rect = actualElement.getBoundingClientRect(),
-        elemTop = rect.top,
-        elemBottom = rect.bottom;
-
-    // Only completely visible elements return true:
-    // var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // Partially visible elements return true:
-    var isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    if (isVisible) {
-        // document.getElementById(el[scrollIdx]).className = 'line-in';
-        console.log(actualElement);
-        scrollIdx++;
-    }
-}
-
-var menuButtons = ['btn-estudio', 'btn-servicios', 'btn-proyectos', 'btn-contacto'],
-    btnSelected = '#e7205b',
-    btnUnselected = '#4a4a4a';
-
-function checkVisibleSection(elem) {
-
-    var actualElement = document.getElementById(`mark-${elem}`),
-        rect = actualElement.getBoundingClientRect(),
-        elemTop = rect.top,
-        elemBottom = rect.bottom;
-
-    // Only completely visible elements return true:
-    // var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
-    // Partially visible elements return true:
-    var isVisible = elemTop < window.innerHeight && elemBottom >= 0;
-    if (isVisible) {
-        paintButton(`btn-${elem}`)
-    }
-
-}
-
-function paintButton(elem) {
-    for (var i = 0; i < menuButtons.length; i++) {
-        if (menuButtons[i] == elem) {
-            document.getElementById(menuButtons[i]).style.color = btnSelected;///TO-DO
-        } else {
-            document.getElementById(menuButtons[i]).style.color = btnUnselected;///TO-DO
-        }
-    }
-}
-
 function onResizeAction() {
     hideOnResize();
-}
-
-
-
-
-
-function onScrollAction() {
-    isScrolledIntoView(['first-line', 'second-line', 'third-line']);
-    checkVisibleSection('estudio');
-    checkVisibleSection('servicios');
-    checkVisibleSection('proyectos');
-    checkVisibleSection('contacto');
 }
 
 window.onload = function(){
