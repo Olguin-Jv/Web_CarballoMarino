1ddab2999fe94fcc183f184a8991952155a45333
OlguinJv <olguin.j.vito@gmail.com>
2019-02-07 19:04:18 -0300

    editando las galerias con las imagenes correspondientes - falta borrar imagenes que sobren y ver para que lado es mejor rotarlas para el responsive

--- js/full-gallery.js
+++ js/full-gallery.js
@@ -71,6 +71,94 @@ function fullToNext(){
 }
 
 
+var isPortrait = true;
+function checkResponsiveImg() {
+
+    if (!fullGalleryState) return;
+    // console.log("gallery-on");
+    checkOrientation();
+
+    var elem = fullGallery.children[fullIdx];
+    var customParam = elem.dataset.responsive;
+    // console.log(customParam);
+    if (customParam == 'false') return;
+
+    // console.log('check portrait')
+    // console.log(`orientation: ${isPortrait}`);
+
+    var elemHeight = elem.getBoundingClientRect().height;
+    var elemWidth = elem.getBoundingClientRect().width;
+
+
+    if (isPortrait) {
+        var aspect = window.innerHeight * 80 / 100;
+        elem.style.width = aspect + 'px'; //CHECK
+        elem.children[0].style = 'transform: rotate(-90deg)';
+        // elem.children[0].style.width = `${elemHeight}px`;
+        // elem.children[0].style.height = `initial`;
+        console.log('90deg');
+    }
+    if (!isPortrait) {
+        elem.style = '100%';
+        elem.children[0].style = 'transform: rotate(0deg)';
+        console.log('0deg');
+    }
+
+    console.log(elem.children[0].getBoundingClientRect().height);
+
+}
+
+
+
+function checkOrientation() {
+    var width = window.innerWidth,
+        height = window.innerHeight;
+
+    if (height > width) {
+        isPortrait = true;
+        console.log('is port');
+    } else if (height < width) {
+        isPortrait = false;
+        console.log('is land');
+    } else if (height < width) {
+        isPortrait = false;
+    }
+
+
+
+}
+
+checkOrientation();
+
+//----------------------------------------------------------------------//
+
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+        return;
+    }
+    switch (event.key) {
+        case "Left": // IE/Edge specific value
+        case "ArrowLeft":
+            console.log('left')
+            fullToPrev();
+            break;
+        case "Right": // IE/Edge specific value
+        case "ArrowRight":
+            console.log('right');
+            fullToNext();
+            break;
+        case "Esc": // IE/Edge specific value
+        case "Escape":
+            console.log('escape');
+            closeFullGallery();
+            // Do something for "esc" key press.
+            break;
+        default:
+            return; // Quit when this doesn't handle the key event.
+    }
+    event.preventDefault();
+}, true);
+
 //para responsivear las imágenes.
 //colocar clases a las etiquetas img. cosa de ubicarlas con el mismo index de la imágen
 //usar custom data para chequear si la img necesita responsivear o no
